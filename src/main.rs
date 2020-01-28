2bbbe17210b8a6105def531aceb6c0b828ff763e
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-01-28 12:39:29 -0500

    generate random number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate rand;
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let n1: u8 = rng.gen();
+    let n2: u64 = rng.gen();
+    println!("{}", n1);
+    println!("{}", n2);
+    println!("{}", rng.gen::<f64>());
+    println!("{}", rng.gen::<i32>());
 }
