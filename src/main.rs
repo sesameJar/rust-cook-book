b280efedc80615859c705a899ab44df645760777
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-01-28 15:09:20 -0500

    Create random password from a set of alphanumeric chars

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,9 @@
 extern crate rand;
 
-use rand::distributions::{Distribution, Standard};
-use rand::Rng;
-
-#[derive(Debug)]
-struct Point {
-    x: i32,
-    y: i32,
-}
-
-impl Distribution<Point> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Point {
-        let (rand_x, rand_y) = rng.gen();
-        Point {
-            x: rand_x,
-            y: rand_y,
-        }
-    }
-}
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let rand_tuple = rng.gen::<(i32, bool, f64)>();
-    let rand_point: Point = rng.gen();
-    println!("Random tuple : {:?}", rand_tuple);
-    println!("Random Point : {:?}", rand_point);
+    let rand_str: String = thread_rng().sample_iter(&Alphanumeric).take(30).collect();
+    println!("{}", rand_str);
 }
