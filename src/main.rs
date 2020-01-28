e90fd6f70194974c95a1b03149834264e1ae34d6
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-01-28 15:41:00 -0500

    Generate random passwords from a set of user-defined charas

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 extern crate rand;
 
-use rand::distributions::Alphanumeric;
-use rand::{thread_rng, Rng};
+use rand::Rng;
 
 fn main() {
-    let rand_str: String = thread_rng().sample_iter(&Alphanumeric).take(30).collect();
-    println!("{}", rand_str);
+    use rand::Rng;
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+    abcdefghijklmnopqrstuvwxyz\
+    0123456789)(*&^%$#@!~";
+
+    const PASSWORD_LEN: usize = 30;
+    let mut rng = rand::thread_rng();
+    let password: String = (0..PASSWORD_LEN)
+        .map(|_| {
+            let idx = rng.gen_range(0, CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+    println!("{:?}", password);
 }
