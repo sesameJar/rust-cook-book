fe94122db7eeef56332315e0db61ddd27b470440
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-01-29 11:13:35 -0500

    sort a vector of floats

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let mut vec = vec![1, 5, 10, 2, 15];
+    let mut vec = vec![1.1, 5.32, 10.1222, 2.2, 15.6, 2.0];
 
-    vec.sort();
-    assert_eq!(vec, vec![1, 2, 5, 10, 15]);
+    vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    assert!(vec == vec![1.1, 2.0, 2.2, 5.32, 10.1222, 15.6]);
 }
