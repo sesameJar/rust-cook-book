b190032efd917e006635fc94b4e2d36b66c28e4a
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-01-31 15:03:26 -0500

    Printing colored text to the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,9 @@
-extern crate clap;
-
-use clap::{App, Arg};
+extern crate ansi_term;
+use ansi_term::Color;
 
 fn main() {
-    let matches = App::new("My test Program")
-        .version("0.0.1")
-        .author("Mehrad Kavian")
-        .about("Some stupid command line App")
-        .arg(
-            Arg::with_name("multiply")
-                .short("m")
-                .long("mul")
-                .takes_value(true)
-                .number_of_values(2)
-                .help("received two number and multiplies them"),
-        )
-        .arg(
-            Arg::with_name("number")
-                .short("n")
-                .long("num")
-                .takes_value(true)
-                .help("your fucking favorite number you mother bitch"),
-        )
-        .get_matches();
-
-    let numbers :Vec<&str> = matches.values_of("multiply").unwrap().collect();
-    println!("{:?}", numbers);
+    println!("This is {} in color, {} in color, {} in color",
+    Color::Red.blink().paint("RED"),
+    Color::Green.bold().underline().italic().paint("GREEN"),
+    Color::Blue.bold().paint("BLUE"));
 }
