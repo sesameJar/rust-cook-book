fb7b94f3b4bddf0b424978a12ead27637347d70c
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-02-04 12:33:11 -0500

    Decompress a tarball white removing a prefix from the paths

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
+#[macro_use]
+extern crate error_chain;
 extern crate flate2;
 extern crate tar;
 
-use std::fs::File;
 use flate2::read::GzDecoder;
+use std::fs::File;
+
+use std::path::PathBuf;
 use tar::Archive;
 
-fn main() -> Result<(), std::io::Error> {
-    let path = "sample.tar.gz";
+error_chain! {
+    foreign_links {
+      Io(std::io::Error);
+      StripPrefixError(::std::path::StripPrefixError);
+    }
+}
 
-    let tar_gz = File::open(path)?;
-    let tar = GzDecoder::new(tar_gz);
-    let mut archive = Archive::new(tar);
-    archive.unpack(".")?;
+fn main() -> Result<()> {
+    let file = File::open("archive.tar.gz")?;
+    let mut archive = Archive::new(GzDecoder::new(file));
+    let prefix = "backup/logs";
+
+    let ent = archive
+        .entries()?
+        .filter_map(|e| e.ok())
+        .map(|mut entry| -> Result<PathBuf> {
+            let path = entry.path()?.strip_prefix(prefix)?.to_owned();
+            entry.unpack(&path)?;
+            Ok(path)
+        })
+        .filter_map(|e| e.ok())
+        .for_each(|x| println!("> {}", x.display()));
 
     Ok(())
 }
