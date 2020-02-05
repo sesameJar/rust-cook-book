0212dea63b7c26936c2ea7b8db0ea871f8a3da9d
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-02-05 12:37:19 -0500

    pass data btw two threads

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
 extern crate crossbeam;
+extern crate crossbeam_channel;
+
+use std::{thread,time};
+use crossbeam_channel::unbounded;
 
 fn main() {
-    let arr = &[1, 35, -4, 10];
-    let max = find_max(arr);
-    assert_eq!(max, Some(35));
-}
-
-fn find_max(arr: &[i32]) -> Option<i32> {
-    const THRESHOLD: usize = 2;
-
-    if (arr.len() <= THRESHOLD) {
-        return arr.iter().cloned().max();
+    let (snd, rcv) = unbounded();
+    let n_msgs= 5;
+    
+    crossbeam::scope(|s|{
+        s.spawn(|_| {
+            for i in 0..n_msgs{
+                snd.send(i).unwrap();
+                thread::sleep(time::Duration::from_millis(100));
+            }
+        });
+    }).unwrap();
+    for _ in 0..n_msgs {
+        let msg = rcv.recv().unwrap();
+        println!("Received {}", msg);
     }
-    let mid = arr.len() / 2;
-    let (left, right) = arr.split_at(mid);
-
-    crossbeam::scope(|s| {
-        let thread_l = s.spawn(|_| find_max(left));
-        let thread_r = s.spawn(|_| find_max(right));
-
-        let max_l = thread_l.join().unwrap()?;
-        let max_r = thread_r.join().unwrap()?;
-
-        Some(max_l.max(max_r))
-    }).unwrap()
 }
