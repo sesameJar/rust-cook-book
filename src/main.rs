d489e952424db6ac5998398d6bf98b841fcfd46f
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-02-05 14:14:35 -0500

    Draw fractal dispatching work to a thread pool

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,97 @@
-extern crate walkdir;
-extern crate ring;
-extern crate num_cpus;
+#[macro_use]
+extern crate error_chain;
 extern crate threadpool;
+extern crate num;
+extern crate num_cpus;
+extern crate image;
 
-use walkdir::WalkDir;
-use std::fs::File;
-use std::io::{BufReader,Read,Error};
-use std::path::Path;
+use std::sync::mpsc::{channel, RecvError};
 use threadpool::ThreadPool;
-use std::sync::mpsc::channel;
-use ring::digest::{Context, Digest, SHA256};
+use num::complex::Complex;
+use image::{ImageBuffer, Pixel, Rgb};
 
-fn is_iso (entry: &Path) -> bool {
-    match entry.extension() {
-        Some(e) if e.to_string_lossy().to_lowercase() == "iso" => true,
-        _ => false,
+error_chain! {
+    foreign_links {
+        MpscRecv(RecvError);
+        Io(std::io::Error);
     }
 }
 
-fn compute_digest<P: AsRef<Path>>(filepath: P) -> Result<(Digest, P), Error> {
-    let mut buf_reader = BufReader::new(File::open(&filepath)?);
-    let mut context = Context::new(&SHA256);
-    let mut buffer= [0; 1024];
+// Function converting intensity values to RGB
+// Based on http://www.efg2.com/Lab/ScienceAndEngineering/Spectra.htm
+fn wavelength_to_rgb(wavelength: u32) -> Rgb<u8> {
+    let wave = wavelength as f32;
 
-    loop {
-        let count = buf_reader.read(&mut buffer)?;
-        if count == 0 {
+    let (r, g, b) = match wavelength {
+        380..=439 => ((440. - wave) / (440. - 380.), 0.0, 1.0),
+        440..=489 => (0.0, (wave - 440.) / (490. - 440.), 1.0),
+        490..=509 => (0.0, 1.0, (510. - wave) / (510. - 490.)),
+        510..=579 => ((wave - 510.) / (580. - 510.), 1.0, 0.0),
+        580..=644 => (1.0, (645. - wave) / (645. - 580.), 0.0),
+        645..=780 => (1.0, 0.0, 0.0),
+        _ => (0.0, 0.0, 0.0),
+    };
+
+    let factor = match wavelength {
+        380..=419 => 0.3 + 0.7 * (wave - 380.) / (420. - 380.),
+        701..=780 => 0.3 + 0.7 * (780. - wave) / (780. - 700.),
+        _ => 1.0,
+    };
+
+    let (r, g, b) = (normalize(r, factor), normalize(g, factor), normalize(b, factor));
+    Rgb::from_channels(r, g, b, 0)
+}
+
+// Maps Julia set distance estimation to intensity values
+fn julia(c: Complex<f32>, x: u32, y: u32, width: u32, height: u32, max_iter: u32) -> u32 {
+    let width = width as f32;
+    let height = height as f32;
+
+    let mut z = Complex {
+        // scale and translate the point to image coordinates
+        re: 3.0 * (x as f32 - 0.5 * width) / width,
+        im: 2.0 * (y as f32 - 0.5 * height) / height,
+    };
+
+    let mut i = 0;
+    for t in 0..max_iter {
+        if z.norm() >= 2.0 {
             break;
         }
-        context.update(&buffer[..count]);
+        z = z * z + c;
+        i = t;
     }
-    Ok((context.finish(), filepath))
+    i
 }
 
-fn main() -> Result<(), Error> {
-    let pool = ThreadPool::new(num_cpus::get());
-    let (tx,rx) = channel();
+// Normalizes color intensity values within RGB range
+fn normalize(color: f32, factor: f32) -> u8 {
+    ((color * factor).powf(0.8) * 255.) as u8
+}
 
-    for entry in WalkDir::new("./")
-        .follow_links(true)
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| !e.path().is_dir() && is_iso(e.path())) {
-            let path  = entry.path().to_owned();
-            let tx = tx.clone();
-            pool.execute(move || {
-                let digest = compute_digest(path);
-                tx.send(digest).expect("Cound not send Data!");
-            });
-        }
-    drop(tx);
-    for t in rx.iter() {
-        let (sha, path) = t?;
-        println!("{:?} {:?}",sha,path);
+fn main() -> Result<()> {
+    let (width, height) = (1920, 1080);
+    let mut img = ImageBuffer::new(width, height);
+    let iterations = 300;
+
+    let c = Complex::new(-0.8, 0.156);
+
+    let pool = ThreadPool::new(num_cpus::get());
+    let (tx, rx) = channel();
+
+    for y in 0..height {
+        let tx = tx.clone();
+        pool.execute(move || for x in 0..width {
+                         let i = julia(c, x, y, width, height, iterations);
+                         let pixel = wavelength_to_rgb(380 + i * 400 / iterations);
+                         tx.send((x, y, pixel)).expect("Could not send data!");
+                     });
     }
+
+    for _ in 0..(width * height) {
+        let (x, y, pixel) = rx.recv()?;
+        img.put_pixel(x, y, pixel);
+    }
+    let _ = img.save("output.png")?;
     Ok(())
 }
