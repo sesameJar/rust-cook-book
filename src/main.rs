1584c25a27f70efd1650c0037a81e63595f675c2
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-02-07 15:19:43 -0500

    Map-reduce in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
 extern crate rayon;
-extern crate rand;
 
-use rand::{Rng, thread_rng};
-use rand::distributions::Alphanumeric;
 use rayon::prelude::*;
 
+struct Person {
+    age : u32,
+}
+
 fn main() {
-    let mut vec = vec![String::new(); 100];
-    vec.par_iter_mut().for_each(|p| {
-        let mut rng = thread_rng();
-        *p = (0..5).map(|_| rng.sample(&Alphanumeric)).collect()
-    });
-    vec.par_sort_unstable();
-    println!("{:? }",vec);
+    let v : Vec<Person> = vec![
+        Person { age: 23 },
+        Person { age: 19 },
+        Person { age: 42 },
+        Person { age: 17 },
+        Person { age: 17 },
+        Person { age: 31 },
+        Person { age: 30 },
+    ];
+    let num_over_30 = v.par_iter().filter(|&n| n.age> 30).count() as f32;
+    let sum_over_30 = v.par_iter().
+    map(|n| n.age)
+    .filter(|&x| x > 30)
+    .reduce(|| 0,|x,y| x+y);
+
+    let alt_sum_30: u32 = v.par_iter()
+        .map(|x| x.age)
+        .filter(|&x| x> 30)
+        .sum();
+
+    let avg_over_30 = sum_over_30 as f32 / num_over_30;
+    let alt_avg_over_30 = alt_sum_30 as f32/ num_over_30;
+
+    assert!((avg_over_30 - alt_avg_over_30).abs() < std::f32::EPSILON);
+    println!("The average age of people older than 30 is {}", (avg_over_30 ) );
+    
 }
