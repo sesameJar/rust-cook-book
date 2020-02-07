a9d8022cc6dd4d2e4cd8f957a3217bf0bb6d4aea
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-02-07 13:15:46 -0500

    Test in Parallel if any or all elements of collection match a given predicate

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ extern crate rayon;
 use rayon::prelude::*;
 
 fn main() {
-    let mut arr = [0, 7, 9, 11];
-    arr.par_iter_mut().for_each(|p| *p -= 1);
-    println!("{:?}", arr);
+    let mut vec = vec![2, 4, 6, 8];
+    println!("{}", vec.par_iter().all(|n| (n % 2) == 0));
+    // print!("{}", vec.par_iter().all(|n| &n <= 8));
+
+    vec.push(9);
+
+    println!("{}",vec.par_iter().all(|n|(n%2)==0));
+    println!("{}", vec.par_iter().all(|n| *n <=8));
 }
