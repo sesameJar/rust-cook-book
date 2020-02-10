728ef62597ca66eadf33d8b2bd0c9dc5a31e7bcb
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-02-10 17:11:11 -0500

    Calculate the Sha-256 digest of a file

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,44 @@
 #[macro_use]
 extern crate error_chain;
-extern crate glob;
-extern crate image;
-extern crate rayon;
+extern crate data_encoding;
+extern crate ring;
 
-use std::fs::create_dir_all;
-use std::path::Path;
-
-use error_chain::ChainedError;
-use glob::{glob_with, MatchOptions};
-use image::{FilterType, ImageError};
-use rayon::prelude::*;
+use data_encoding::HEXUPPER;
+use ring::digest::{Context, Digest, SHA256};
+use std::fs::File;
+use std::io::{BufReader, Read, Write};
 
 error_chain! {
     foreign_links {
-        Image(ImageError);
         Io(std::io::Error);
-        Glob(glob::PatternError);
+        Decode(data_encoding::DecodeError);
     }
+}
+
+fn sha256_digest<R: Read>(mut reader: R) -> Result<Digest> {
+    let mut context = Context::new(&SHA256);
+    let mut buffer = [0; 1204];
+    loop {
+        let count = reader.read(&mut buffer)?;
+        if count == 0 {
+            break;
+        }
+        context.update(&buffer[..count]);
+    }
+
+    Ok(context.finish())
 }
 
 fn main() -> Result<()> {
-    let options: MatchOptions = Default::default();
-    let files: Vec<_> = glob_with("./images/*.jpg", options)?
-        .filter_map(|x| x.ok())
-        .collect();
+    let path = "file.txt";
+    let mut file = File::create(path)?;
+    write!(file, "Hey bitches");
 
-    if files.len() == 0 {
-        bail!("No fucking JPG file dumbass");
-    }
+    let input = File::open(path)?;
+    let buffer = BufReader::new(input);
+    let digest = sha256_digest(buffer)?;
 
-    let thumb_dir = "images/thumbnails";
-    create_dir_all(thumb_dir)?;
-    println!("Saving {} thumbnails into '{}'...", files.len(), thumb_dir);
-
-    let image_failures : Vec<_> = files
-    .par_iter()
-    .map(|path| {
-        make_thumbnail(path, thumb_dir, 300)
-        .map_err(|e| e.chain_err(|| path.display().to_string()))
-    })
-    .filter_map(|x| x.err())
-    .collect();
-
-    image_failures.iter().for_each(|x| println!("{}", x.display_chain()));
-    println!("{} thumbnails save successfully", files.len() - image_failures.len());
+    println!("{}", HEXUPPER.encode(digest.as_ref()));
 
     Ok(())
-    
-}
-
-fn make_thumbnail<PA, PB>(original: PA, thumb_dir: PB, longest_edge: u32) -> Result<()>
-where
-    PA: AsRef<Path>,
-    PB: AsRef<Path>,
-{
-    let img = image::open(original.as_ref())?;
-    let file_path = thumb_dir.as_ref().join(original);
-
-    Ok(img.resize(longest_edge, longest_edge, FilterType::Nearest)
-        .save(file_path)?)
 }
