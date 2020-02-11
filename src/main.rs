c8379ea03f7f15af81fb56e580068ee1f320df10
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-02-11 14:23:06 -0500

    usage of data-encoding in sign a message with hmac

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
+extern crate data_encoding;
 extern crate ring;
 
 use ring::error::Unspecified;
 use ring::rand::SecureRandom;
 use ring::{digest, hmac, rand};
+use data_encoding::HEXUPPER;
 
 fn main() -> Result<(), Unspecified> {
     let mut key_value = [0u8; 48];
     let rng = rand::SystemRandom::new();
     rng.fill(&mut key_value);
-    
 
     let key = hmac::Key::new(hmac::HMAC_SHA256, &key_value);
     let raw_sig = hmac::sign(&key, "HEY".as_bytes());
-    println!("{:?}", raw_sig.as_ref());
+    println!("{:?}", HEXUPPER.encode(raw_sig.as_ref()));
 
-    hmac::verify(&key, "HE1Y".as_bytes(), &raw_sig.as_ref())?;
+    hmac::verify(&key, "HEY".as_bytes(), &raw_sig.as_ref())?;
 
     Ok(())
 }
