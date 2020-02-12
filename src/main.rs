4652ecf78238cdcb4b80aa655cee7f2f847fa95f
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-02-12 13:48:56 -0500

    Define and operate on a type represented as a bitfield

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,51 @@
-extern crate data_encoding;
-extern crate ring;
+#[macro_use]
+extern crate bitflags;
 
-use data_encoding::HEXUPPER;
-use ring::error::Unspecified;
-use ring::rand::SecureRandom;
-use ring::{ pbkdf2, rand};
-use std::num::NonZeroU32;
+use std::fmt;
 
-fn main () -> Result<(), Unspecified> {
-    let password= "NO WAY YOU CAN GUESS THIS BITCH!";
-    const CREDENTIAN_LEN : usize = ring::digest::SHA512_OUTPUT_LEN;
-    let n_iter = NonZeroU32::new(10).unwrap();
-    let rng = rand::SystemRandom::new();
-    let mut salt = [0u8; CREDENTIAN_LEN];
+bitflags! {
+    struct MyFlags: u32 {
+        const FLAG_A       = 0b00000001;
+        const FLAG_B       = 0b00000010;
+        const FLAG_C       = 0b00000100;
+        const FLAG_ABC     = Self::FLAG_A.bits
+                           | Self::FLAG_B.bits
+                           | Self::FLAG_C.bits;
+    }
+}
 
-    rng.fill(&mut salt);
+impl MyFlags {
+    pub fn clear(&mut self) -> &mut MyFlags {
+        self.bits = 0;
+        self
+    }
+}
 
-    let mut hashed_pass = [0u8; CREDENTIAN_LEN];
-    pbkdf2::derive(pbkdf2::PBKDF2_HMAC_SHA512, n_iter, &salt, password.as_bytes(), &mut hashed_pass);
+impl fmt::Display for MyFlags {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:032b}", self.bits)
+    }
+}
 
-    println!("SALT: {}", HEXUPPER.encode(&salt));
-    println!("PBKDF2 : {}", HEXUPPER.encode(&hashed_pass));
+fn main() {
+    let e1 = MyFlags::FLAG_A | MyFlags::FLAG_C;
+    let e2 = MyFlags::FLAG_B | MyFlags::FLAG_C;
+    assert_eq!((e1 | e2), MyFlags::FLAG_ABC);
+    assert_eq!((e1 & e2), MyFlags::FLAG_C);
+    assert_eq!((e1 - e2), MyFlags::FLAG_A);
+    assert_eq!(!e2, MyFlags::FLAG_A);
 
-    //lets Verify
-    let should_ok = pbkdf2::verify(pbkdf2::PBKDF2_HMAC_SHA512, n_iter, &salt, password.as_bytes(), &hashed_pass);
-    assert!(should_ok.is_ok());
+    let mut flags = MyFlags::FLAG_ABC;
+    assert_eq!(format!("{}", flags), "00000000000000000000000000000111");
+    assert_eq!(
+        format!("{}", flags.clear()),
+        "00000000000000000000000000000000"
+    );
+    assert_eq!(format!("{:?}", MyFlags::FLAG_B), "FLAG_B");
+    assert_eq!(
+        format!("{:?}", MyFlags::FLAG_A | MyFlags::FLAG_B),
+        "FLAG_A | FLAG_B"
+    );
 
-    let not_ok = pbkdf2::verify(pbkdf2::PBKDF2_HMAC_SHA512, n_iter, &salt, "SOME WRONG PASSWORD".as_bytes(), &hashed_pass);
-    assert!(!not_ok.is_ok());
-    Ok(())
+    println!("{}", MyFlags::FLAG_A);
 }
